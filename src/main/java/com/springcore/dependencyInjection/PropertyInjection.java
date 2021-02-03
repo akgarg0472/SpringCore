39f7d33b9736d426383985ccd94041c4b309a0d0
Akhilesh Garg <akgarg0472@gmail.com>
2021-02-03 08:03:11 +0530

    update PropertyInjection.java file

--- src/main/java/com/springcore/dependencyInjection/PropertyInjection.java
+++ src/main/java/com/springcore/dependencyInjection/PropertyInjection.java
@@ -1,5 +1,6 @@
 package com.springcore.dependencyInjection;
 
+import com.springcore.dependencyInjection.primitive.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,12 +10,12 @@ public class PropertyInjection {
         ApplicationContext ac = new ClassPathXmlApplicationContext("dependencyInjectionConfigs/PIConfig.xml");
 
         // requesting object of Student from IOC container which is accessed using ApplicationContext
-//        Student studentOne = (Student) ac.getBean("studentOne");
-//        System.out.println(studentOne);
+        Student studentOne = (Student) ac.getBean("studentOne");
+        System.out.println(studentOne);
 
         // value set using p schema
-//        Student studentP = (Student) ac.getBean("studentP");
-//        System.out.println(studentP);
+        Student studentP = (Student) ac.getBean("studentP");
+        System.out.println(studentP);
 
         // value set using collection framework classes
         com.springcore.dependencyInjection.collection.Student studentCollection = (com.springcore.dependencyInjection.collection.Student) ac.getBean("studentCollection");
@@ -22,7 +23,7 @@ public class PropertyInjection {
         System.out.println(studentCollection.getPhones().getClass().getName());
 
         // value set using reference type injection
-//        com.springcore.dependencyInjection.reference.Student studentReference = (com.springcore.dependencyInjection.reference.Student) ac.getBean("studentReference");
-//        System.out.println(studentReference);
+        com.springcore.dependencyInjection.reference.Student studentReference = (com.springcore.dependencyInjection.reference.Student) ac.getBean("studentReference");
+        System.out.println(studentReference);
     }
 }
